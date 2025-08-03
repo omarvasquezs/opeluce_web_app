0de809ca6ab78a80e7c9fe45ffc199825f362cfd
Omar Vásquez <omarvs91@gmail.com>
2025-08-02 19:53:49 -0500

    refactor: Remove unused font links and ensure Font Awesome is loaded correctly.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,9 +8,8 @@
         <title>Opeluce | Sistema de Gestion</title>
         <link rel="icon" type="image/png" href="/images/icon.png">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
